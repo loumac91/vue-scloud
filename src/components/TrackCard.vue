841dcec89b188d76df648b95c0e69e78f4c86d7b
loumac91 <loumac91@googlemail.com>
2020-01-19 17:07:51 +0000

    feat - base track card

---
+++ src/components/TrackCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="card">
+    <div class="track-card__image"></div>
+    <div class="track-card__content"></div>
+    <p>Card</p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "TrackCard",
+  props: {
+    track: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted() {
+    console.log("t", this.track);
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,12 +5,25 @@
     <div class="card">
       <h1>Hello Vorld</h1>
     </div>
+    <TrackCard :track="track" />
   </div>
 </template>
 
 <script>
+import TrackCard from "@/components/TrackCard.vue";
+
+import { fakeApiResponse } from "@/fakeData";
+
 export default {
   name: "Home",
+  components: {
+    TrackCard
+  },
+  data() {
+    return {
+      track: fakeApiResponse
+    };
+  },
   methods: {
     handleClick() {
       this.$store.dispatch("FETCH_TRACKS", "travis orbin");
