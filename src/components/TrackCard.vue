9fe32c4705980e3a0c8bf9c50db2b7c41ba45fed
loumac91 <loumac91@googlemail.com>
2020-02-08 16:57:08 +0000

    add some icons

--- src/components/TrackCard.vue
+++ src/components/TrackCard.vue
@@ -1,11 +1,14 @@
 <template>
-  <li class="track-card">
+  <li class="track-card drop-shadow-bottom-right">
     <div class="track-card__image">
       <img :src="track.artworkUrl" />
     </div>
     <div class="track-card__content">
-      <h1>{{ track.title }}</h1>
-      <p>{{ track.username }}</p>
+      <p class="track-card__title">{{ track.title }}</p>
+      <p class="track-card__username">{{ track.username }}</p>
+    </div>
+    <div class="track-card__controls">
+      <button class="btn btn-icon"><i class="fas fa-play"></i></button>
     </div>
   </li>
 </template>
@@ -18,16 +21,25 @@ export default {
       type: Object,
       required: true
     }
+  },
+  computed: {
+    isPlaying() {
+      return true;
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/styles/variables.scss";
+
 .track-card {
   display: flex;
   flex-direction: column;
-  border-radius: 30px;
+  border-radius: 16px;
   flex: 0 1 calc(25% - 1em);
+  background-color: $base-card-color;
+  margin-bottom: 16px;
 
   @media (max-width: 768px) {
     flex: 0 1 calc(50% - 1em);
@@ -35,16 +47,24 @@ export default {
 
   &__image {
     position: relative;
+    height: auto;
 
     & > img {
+      // border: 0;
+      border-radius: 16px 16px 0 0;
+      height: auto;
       max-width: 100%;
-      border: 0;
       vertical-align: middle;
     }
   }
 
   &__content {
-    color: #fff;
+    padding: 8px;
+    color: black;
   }
+}
+
+.drop-shadow-bottom-right {
+  filter: drop-shadow(10px 10px 10px #a6abbd);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,30 @@
 <template>
   <main class="page-container">
-    <h1>Home</h1>
-    <div class="card">
-      <h1>Hello Vorld</h1>
+    <div class="canvas">
+      <div class="ds-card-container">
+        <div class="ds-card drop-shadow-bottom-right">
+          <div class="ds-card__image">
+            <img :src="'https://unsplash.it/800/600?image=59'" />
+          </div>
+          <h1>HELLO</h1>
+          <i class="fas fa-play"></i>
+          <br />
+          <i class="fas fa-pause"></i>
+          <br />
+          <i class="fas fa-stop"></i>
+          <br />
+          <i class="fas fa-step-forward"></i>
+          <br />
+          <i class="fas fa-step-backward"></i>
+          <br />
+          <i class="fas fa-plus"></i>
+          <br />
+          <i class="fas fa-minus"></i>
+        </div>
+      </div>
     </div>
+
+    <h1>Home</h1>
     <TrackGrid :tracks="getTracks" />
   </main>
 </template>
@@ -23,4 +44,44 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "@/styles/variables.scss";
+
+.card-container {
+  filter: drop-shadow(-10px -10px 10px #fafbff);
+}
+
+.ds-card {
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  width: 250px;
+  height: 250px;
+  background-color: $base-card-color;
+
+  &__image {
+    position: relative;
+    height: auto;
+
+    > img {
+      height: auto;
+      max-width: 100%;
+      vertical-align: middle;
+    }
+  }
+}
+
+.drop-shadow-bottom-right {
+  filter: drop-shadow(10px 10px 10px #a6abbd);
+}
+
+.canvas {
+  border-radius: 10px;
+  width: 300px;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ebecf0;
+}
+</style>
